7f8984456c6fdc87ace33f89a1e8599a7960b183
PiyushChaukade <93372962+PiyushChaukade@users.noreply.github.com>
2022-06-05 22:41:39 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+<calculator-main></calculator-main>
+
+</template>
+
+<script>
+import calculatorMain from './components/calculatorMain.vue'
+export default {
+  name: 'App',
+  components: {
+    calculatorMain
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/calculatorMain.vue
@@ -0,0 +1,231 @@
+<template>
+    <section>
+          <h1>Calculator</h1>
+
+            <input id="input" :value="uservalue">
+        <div>
+          
+
+            <div id="block1">
+
+                <div id="numbers">
+                    <button @click="modulo">%</button>
+                     <button @click="clearOnce">back</button>
+                     <button @click="clearAll">clear all</button>
+                    <button v-for="i in 10" :key="i" @click="getNumber(10-i)" >{{10-i}} </button>
+                    <button @click="dot('.')">.</button>
+                     <button >theme</button>
+
+                </div>
+            
+            </div>
+
+             <div id="block1"  >
+                    
+                    <div id="operators" > 
+
+                     <button @click="plus(i)">+</button>
+                     <button @click="minus(i)">-</button>
+                     <button @click="multiple(i)">*</button>
+                     <button @click="divide(i)">/</button>
+                     <button @click="equalTo(i)">=</button>
+
+                     
+
+                    </div>
+                 
+            </div>
+        </div>
+    </section>
+        
+</template>
+
+<script>
+
+export default {
+   data(){
+       return {
+           previous:null,
+
+           inital:0,
+           uservalue:'0',
+           operator:null,
+           operatorClicked:false,
+       }
+   },
+   methods:{
+       getNumber(i){
+      
+        if(this.uservalue=='0'){
+
+          if( this.uservalue=='0' && i=='+' || i=='-' || i=='/' || i=='*' || i=='%' || i=='.'){
+               this.uservalue += i;
+               //console.log("if")
+          } 
+          else if(this.uservalue=='0'){
+         this.uservalue =''
+         this.uservalue += i
+               //console.log("else if")
+          }
+        
+       }
+       else{
+              this.uservalue += i
+            //    console.log("else")
+          }   
+       },
+       checkoperators(x){
+           
+            if(this.uservalue[this.uservalue.length-1]=='+' || this.uservalue[this.uservalue.length-1]=='-' || this.uservalue[this.uservalue.length-1]=='*' || this.uservalue[this.uservalue.length-1]=='/' ||
+             this.uservalue[this.uservalue.length-1]=='%' ||  this.uservalue[this.uservalue.length-1]=='.'){
+             //
+            }
+            else{
+                this.uservalue+=x
+            }
+       },
+
+       plus(){
+          this.checkoperators('+')
+    
+       },
+       minus(){
+           this.checkoperators('-')
+
+           },
+       divide(){
+           this.checkoperators('/')
+         
+           },
+       modulo(){
+           this.checkoperators('%')
+           this.uservalue = `${parseFloat(this.uservalue)/100}`
+
+           },
+       multiple(){
+           this.checkoperators('*')
+           },
+       dot(){
+               this.checkoperators('.')
+            
+           },
+       clearAll(){
+           this.uservalue=''
+           this.uservalue='0'
+          },
+       clearOnce(){
+      
+     
+         this.uservalue = this.uservalue.slice(0,(this.uservalue.length-1))
+         //   console.log(this.count)
+          if(this.uservalue==''){
+          this.uservalue='0'
+          }
+      
+       },
+       equalTo(){
+      
+        let operatorStore=[];
+        let getAns=[]   
+        console.log(this.uservalue)
+
+        let ans= (this.uservalue.split(/[+-/*]/))
+        
+        console.log("ans",ans)
+       
+
+        for(let i=0; i<this.uservalue.length; i++){
+
+            if(this.uservalue[i]=='+'){
+                operatorStore.push('+')
+            }
+            else if(this.uservalue[i]=='-'){
+                operatorStore.push('-')
+            }
+            else if(this.uservalue[i]=='*'){
+                operatorStore.push('*')
+            }
+            else if(this.uservalue[i]=='/'){
+                operatorStore.push('/')
+            } 
+        }
+        console.log("operator store",operatorStore)
+
+        for(let i=0; i<ans.length; i++){
+         if(i%2==1){
+         getAns.push(ans[i])
+         getAns.push(operatorStore[i])
+
+         }
+         else{
+         getAns.push(ans[i])
+         getAns.push(operatorStore[i])
+
+         }
+        }
+        console.log("getAns",getAns)
+
+        this.inital += Number(getAns[0])
+        console.log(this.inital)
+
+         for(let i=0; i<getAns.length; i++){
+
+            if(getAns[i]=='+'){
+            this.inital += Number(getAns[i+1])
+            }
+            else if(getAns[i]=='-'){
+            this.inital -= Number(getAns[i+1])
+            }
+            else if(getAns[i]=='*'){
+            this.inital *= Number(getAns[i+1])
+
+            }
+            else if(getAns[i]=='/'){
+            this.inital /= Number(getAns[i+1])
+
+            } 
+        }
+        console.log(this.inital)
+
+        this.uservalue = this.inital.toString();
+        this.inital=0;
+
+       },
+   },
+}
+</script>
+
+<style scoped>
+#block1{
+    display: inline-block;
+    vertical-align: top;
+     gap: 5%;
+    margin: 25px 15px 0 15px;
+
+}
+#numbers{
+display: grid;
+grid-template-columns: repeat(3,1fr);
+vertical-align: top;
+gap: 5%;
+}
+#numbers>button{
+   
+padding: 10px;
+}
+#operators>button{
+    margin-bottom: 38%;
+    display: block;
+    text-align: center;
+    width: 40px;
+    height: 39px;
+    
+}
+#input{
+    text-align: right;
+    padding-right: 10px;
+    height: 40px;
+    width: 290px;
+}
+
+</style>
